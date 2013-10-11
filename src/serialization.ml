ab5f3c874c13c03933afea02eb914f73534361d5
fmp88 <florian.pichlmeier@mytum.de>
2013-10-11 20:43:47 +0200

    added serialization to this branch

---
+++ src/serialization.ml
@@ -0,0 +1,30 @@
+module type Ser = sig
+
+  type t
+
+  val decode : string -> t
+
+  val encode : t -> string 
+
+end
+
+module Make (S : Ser ) = struct 
+ 
+  type t = S.t
+  
+  let send socket msg = Str.send socket (S.encode msg)
+
+  let sendm socket msg = Str.sendm socket (S.encode msg)
+
+  let sendx socket msg_list = Str.sendx socket (List.map (fun msg -> S.encode msg) msg_list)
+
+  let recv socket = S.decode (Str.recv socket)
+
+end
+
+(*
+module Json = Make(struct 
+                   type t = Json.t 
+                   let decode = Json.decode 
+                   let encode Json.encode end)
+*)
